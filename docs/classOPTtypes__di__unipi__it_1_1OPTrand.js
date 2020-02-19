9282f85f7f6625a91ccdc7fd4f8c65b80c38cd09
Antonio Frangioni <frangio@di.unipi.it>
2020-02-19 20:16:47 +0100

    updated documentation

--- docs/classOPTtypes__di__unipi__it_1_1OPTrand.js
+++
@@ -1,6 +0,0 @@
-var classOPTtypes__di__unipi__it_1_1OPTrand =
-[
-    [ "OPTrand", "classOPTtypes__di__unipi__it_1_1OPTrand.html#a1072daef6b56695678f5ebb5096e7953", null ],
-    [ "rand", "classOPTtypes__di__unipi__it_1_1OPTrand.html#afb2af72eb05fcf452da2d1c3a2e0b426", null ],
-    [ "srand", "classOPTtypes__di__unipi__it_1_1OPTrand.html#ac35b0f656fed40605c1c8ceda412f185", null ]
-];
--- docs/group__OPTUtils__FUNCTIONS.js
+++
@@ -1,4 +0,0 @@
-var group__OPTUtils__FUNCTIONS =
-[
-    [ "DfltdSfInpt", "group__OPTUtils__FUNCTIONS.html#gaf4f201a8b54197416f25f062edc98ca6", null ]
-];
