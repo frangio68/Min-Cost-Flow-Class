754cdbba348c2eb33874f5f57c1c8b2dc3402d83
Donato Meoli <donato.meoli.95@gmail.com>
2023-07-17 15:57:35 +0200

    added Windows support for CPLEX

--- cmake/FindCPLEX.cmake
+++ cmake/FindCPLEX.cmake
@@ -35,18 +35,73 @@ include(FindPackageHandleStandardArgs)
 # - a list of possible ILOG directories
 # - a list of possible lib suffixes to find the library
 
-if (APPLE)
-    # macOS (usually /Applications)
-    set(CPLEX_ILOG_DIRS /Applications)
-    set(CPLEX_LIB_PATH_SUFFIXES
-            lib/${ARCH}_darwin9_gcc4.0/static_pic
-            lib/${ARCH}_osx/static_pic)
+if (UNIX)
+    if (APPLE)
+        # macOS (usually /Applications)
+        set(CPLEX_ILOG_DIRS /Applications)
+        set(CPLEX_LIB_PATH_SUFFIXES
+                lib/${ARCH}_darwin9_gcc4.0/static_pic
+                lib/${ARCH}_osx/static_pic)
+    else ()
+        # Other Unix-based systems (usually /opt/ibm/ILOG)
+        set(CPLEX_ILOG_DIRS /opt/ibm/ILOG /opt/IBM/ILOG)
+        set(CPLEX_LIB_PATH_SUFFIXES
+                lib/${ARCH}_sles10_4.1/static_pic
+                lib/${ARCH}_linux/static_pic)
+    endif ()
 else ()
-    # Other Unix-based systems (usually /opt/ibm/ILOG)
-    set(CPLEX_ILOG_DIRS /opt/ibm/ILOG /opt/IBM/ILOG)
-    set(CPLEX_LIB_PATH_SUFFIXES
-            lib/${ARCH}_sles10_4.1/static_pic
-            lib/${ARCH}_linux/static_pic)
+    # Windows (usually C:/Program Files/IBM/ILOG)
+    set(CPLEX_ILOG_DIRS "C:/Program Files/IBM/ILOG")
+    if (ARCH EQUAL x86)
+        set(CPLEX_ILOG_DIRS
+                "C:/Program Files (x86)/IBM/ILOG" ${CPLEX_ILOG_DIRS})
+    endif ()
+
+    # Amended for VS and its various toolsets
+    # https://cmake.org/cmake/help/v3.11/variable/MSVC_VERSION.html
+    # Can use GREATER_EQUAL instead of the mess below if cmake version >= 3.7
+    #if (NOT (MSVC_VERSION LESS 1930)) # VS 17.0 (v143 toolset)
+    #    set(CPLEX_LIB_PATH_SUFFIXES
+    #            lib/${ARCH}_windows_msvc17/stat_mda)
+    #    set(CPLEX_LIB_PATH_SUFFIXES_DEBUG
+    #            lib/${ARCH}_windows_msvc17/stat_mdd)
+    #elseif (NOT (MSVC_VERSION LESS 1920)) # VS 16.0 (v142 toolset)
+    #    set(CPLEX_LIB_PATH_SUFFIXES
+    #            lib/${ARCH}_windows_msvc16/stat_mda)
+    #    set(CPLEX_LIB_PATH_SUFFIXES_DEBUG
+    #            lib/${ARCH}_windows_msvc16/stat_mdd)
+    #elseif (NOT (MSVC_VERSION LESS 1910)) # VS 15.0 (v141 toolset)
+    #    set(CPLEX_LIB_PATH_SUFFIXES
+    #            lib/${ARCH}_windows_msvc15/stat_mda)
+    #    set(CPLEX_LIB_PATH_SUFFIXES_DEBUG
+    #            lib/${ARCH}_windows_msvc15/stat_mdd)
+    #else
+    if (NOT (MSVC_VERSION LESS 1900)) # VS 14.0 (v140 toolset)
+        set(CPLEX_LIB_PATH_SUFFIXES
+                lib/${ARCH}_windows_msvc14/stat_mda)
+        set(CPLEX_LIB_PATH_SUFFIXES_DEBUG
+                lib/${ARCH}_windows_msvc14/stat_mdd)
+    elseif (NOT (MSVC_VERSION LESS 1800)) # VS 12.0 (v120 toolset)
+        set(CPLEX_LIB_PATH_SUFFIXES
+                lib/${ARCH}_windows_msvc12/stat_mda)
+        set(CPLEX_LIB_PATH_SUFFIXES_DEBUG
+                lib/${ARCH}_windows_msvc12/stat_mdd)
+    elseif (NOT (MSVC_VERSION LESS 1700)) # VS 11.0 (v110 toolset)
+        set(CPLEX_LIB_PATH_SUFFIXES
+                lib/${ARCH}_windows_msvc11/stat_mda)
+        set(CPLEX_LIB_PATH_SUFFIXES_DEBUG
+                lib/${ARCH}_windows_msvc11/stat_mdd)
+    elseif (NOT (MSVC_VERSION LESS 1600)) # VS 10.0 (v100 toolset)
+        set(CPLEX_LIB_PATH_SUFFIXES
+                lib/${ARCH}_windows_msvc10/stat_mda)
+        set(CPLEX_LIB_PATH_SUFFIXES_DEBUG
+                lib/${ARCH}_windows_msvc10/stat_mdd)
+    elseif (NOT (MSVC_VERSION LESS 1500)) # VS  9.0 (v90 toolset)
+        set(CPLEX_LIB_PATH_SUFFIXES
+                lib/${ARCH}_windows_msvc09/stat_mda)
+        set(CPLEX_LIB_PATH_SUFFIXES_DEBUG
+                lib/${ARCH}_windows_msvc09/stat_mdd)
+    endif ()
 endif ()
 
 # ----- Find the path to CPLEX Studio --------------------------------------- #
@@ -88,14 +143,48 @@ else ()
               PATHS ${CPLEX_DIR}/include
               DOC "CPLEX include directory.")
 
-    # ----- Find the CPLEX library ------------------------------------------ #
-    # Note that find_library() creates a cache entry
-    find_library(CPLEX_LIBRARY
-                 NAMES cplex
-                 PATHS ${CPLEX_DIR}
-                 PATH_SUFFIXES ${CPLEX_LIB_PATH_SUFFIXES}
-                 DOC "CPLEX library.")
-    set(CPLEX_LIBRARY_DEBUG ${CPLEX_LIBRARY})
+    # ----- Macro: find_win_cplex_library ----------------------------------- #
+    # On Windows the version is appended to the library name which cannot be
+    # handled by find_library, so here a macro to search manually.
+    macro(find_win_cplex_library var path_suffixes)
+        foreach (s ${path_suffixes})
+            file(GLOB CPLEX_LIBRARY_CANDIDATES "${CPLEX_DIR}/${s}/cplex*.lib")
+            if (CPLEX_LIBRARY_CANDIDATES)
+                list(GET CPLEX_LIBRARY_CANDIDATES 0 ${var})
+                break()
+            endif ()
+        endforeach ()
+        if (NOT ${var})
+            set(${var} NOTFOUND)
+        endif ()
+    endmacro()
+
+    if (UNIX)
+        # ----- Find the CPLEX library ------------------------------------------ #
+        # Note that find_library() creates a cache entry
+        find_library(CPLEX_LIBRARY
+                NAMES cplex
+                PATHS ${CPLEX_DIR}
+                PATH_SUFFIXES ${CPLEX_LIB_PATH_SUFFIXES}
+                DOC "CPLEX library.")
+        set(CPLEX_LIBRARY_DEBUG ${CPLEX_LIBRARY})
+
+    elseif (NOT CPLEX_LIBRARY)
+
+        # Library
+        find_win_cplex_library(CPLEX_LIB "${CPLEX_LIB_PATH_SUFFIXES}")
+        set(CPLEX_LIBRARY ${CPLEX_LIB})
+
+        # Debug library
+        find_win_cplex_library(CPLEX_LIB "${CPLEX_LIB_PATH_SUFFIXES_DEBUG}")
+        set(CPLEX_LIBRARY_DEBUG ${CPLEX_LIB})
+
+        # DLL
+        if (CPLEX_LIBRARY MATCHES ".*/(cplex.*)\\.lib")
+            file(GLOB CPLEX_DLL_ "${CPLEX_DIR}/bin/*/${CMAKE_MATCH_1}.dll")
+            set(CPLEX_DLL ${CPLEX_DLL_})
+        endif ()
+    endif ()
 
     # ----- Parse the version ----------------------------------------------- #
     if (CPLEX_INCLUDE_DIR)
